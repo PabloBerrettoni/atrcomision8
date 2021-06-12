59754b5a81c1788234a8e3f4e95e5cb6de85968c
BrianX340 <brianx340@gmail.com>
2021-06-12 14:56:35 -0300

    Se crea archivo models

---
+++ models.js
@@ -0,0 +1,41 @@
+
+
+class Estudiante {
+    constructor(){
+        this.name = null
+        this.lastName = null
+        this.age = null
+        this.username = null
+        this.password = null
+        this.teacher = null
+        this.materias = null
+    }
+
+    setName(newName){
+        this.name = newName
+    }
+
+    setLastName(newLastName){
+        this.lastName = newLastName
+    }
+
+    setAge(newAge){
+        this.age = newAge
+    }
+
+    setUsername(newUsername){
+        this.username = newUsername
+    }
+
+    setPassword(newPassword){
+        this.password = newPassword
+    }
+
+    setTeacher(newTeacher){
+        this.teacher = newTeacher
+    }
+
+    setMaterias(newMateria){
+        this.materia = newMateria
+    }
+}
