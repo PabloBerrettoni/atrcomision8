0bae6cc0a6cfdab94368f2c6cc35d37257693465
BrianX340 <brianx340@gmail.com>
2021-06-12 14:59:45 -0300

    Se crea clase teacher

--- models.js
+++ models.js
@@ -39,3 +39,43 @@ class Estudiante {
         this.materia = newMateria
     }
 }
+
+class Teacher {
+    constructor(){
+        this.name = null
+        this.lastName = null
+        this.age = null
+        this.username = null
+        this.password = null
+        this.students = null
+        this.materias = null
+    }
+
+    setName(newName){
+        this.name = newName
+    }
+
+    setLastName(newLastName){
+        this.lastName = newLastName
+    }
+
+    setAge(newAge){
+        this.age = newAge
+    }
+
+    setUsername(newUsername){
+        this.username = newUsername
+    }
+
+    setPassword(newPassword){
+        this.password = newPassword
+    }
+
+    setStudents(newStudent){
+        this.students = newStudent
+    }
+
+    setMaterias(newMateria){
+        this.materia = newMateria
+    }
+}
