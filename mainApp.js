1715d35f83024d5a5c2c358c03af92ded8dd57be
BrianX340 <brianx340@gmail.com>
2021-06-13 17:16:17 -0300

    asd

--- mainApp.js
+++ mainApp.js
@@ -1,13 +1,27 @@
-
-
-
-
+//cargando dependencias
+const express = require('express');
+const bodyParser = require('body-parser');
+const model = require('./models')
+//const MySQL = require('./mysql/mysql')
+//const mysql = new MySQL();
 
 //inicio de la app
+const app = express();
+app.use(bodyParser.json());
 
-//cargando dependencias
-const model = require('./models')
 
+//manejo de rutas
+app.get('/', (req, res) => {
+    res.send('Holanda desde express');
+ });
+
+
+
+/*
+//credenciales de mysql
+// nombre_db: usuarios_db
+//  user: node_user
+//  pass: comision8
 
 //declaramos nuevo Estudiante
 let usuarioNuevo = new model.Student()
@@ -18,3 +32,4 @@ usuarioNuevo.setUsername('BrianX340')
 
 //mostramos el usuario creado
 console.log(usuarioNuevo)
+*/
